db52b4a1c24ae252282129af7e98001c54a42619
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2022-01-15 22:58:58 +0100

    update package info

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="metadata_mp3", # Replace with your own username
-    version="1.0",
+    name="metadata_mp3",
+    version="1.1",
+    license="MIT",
     description="small package to manage MP3 metadata using mutagen",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bartek56/sampleproject",
+    url="https://github.com/bartek56/metadata_mp3",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
